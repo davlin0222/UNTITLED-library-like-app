5e1ecc9b2e1bb9f53785d6dd2551e5369d1b042b
DavLin <davlin@creatorise.com>
2021-09-22 08:56:46 +0200

    Add manual_testing as console application

---
+++ Manual_testing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Manual_testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
