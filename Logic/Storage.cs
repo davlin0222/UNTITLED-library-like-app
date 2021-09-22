026b33ff61df40cae112d37a384b3740ca4d89d3
DavLin <davlin@creatorise.com>
2021-09-22 10:39:47 +0200

    Create storage as class with list

---
+++ Logic/Storage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Logic
+{
+    public class Storage
+    {
+        List<string> media = new List<string>();
+
+        public void storage_add(string value) {
+            media.Add(value);
+        }
+
+        public override string ToString() {
+            return string.Join(",", media);;
+        }
+}
+}
