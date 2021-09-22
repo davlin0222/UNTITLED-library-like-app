ae4fca13311f57abcce848f702ee70711b922e03
DavLin <davlin@creatorise.com>
2021-09-22 11:44:37 +0200

    Add Media with title property

---
+++ Logic/Media.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Logic
+{
+    public class Media
+    {
+        public string Title{get; set;}
+
+        public Media(string title) {
+            this.Title = title;
+        }
+    }
+}
--- Manual_testing/Program.cs
+++ Manual_testing/Program.cs
@@ -7,11 +7,23 @@ namespace Manual_testing
     {
         static void Main(string[] args)
         {
+            test_storage();
+            test_media();
+        }
+
+        private static void test_storage() {
             Storage media = new Storage();
 
             media.storage_add("Item 1");
 
             Console.WriteLine(media.ToString());
+        }
+
+        private static void test_media() {
+            Media book = new Media("The Ranger's Apprentice");
+            Console.WriteLine(book.Title.ToString());
+            book.Title = "The Ranger's Apprentice 2";
+            Console.WriteLine(book.Title.ToString());
         }
     }
 }
